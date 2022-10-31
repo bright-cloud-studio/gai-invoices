9936acaa231d3f8c32586473e92d384035ed4ba6
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-10-31 13:21:28 -0400

    Create config.php

---
+++ system/modules/gai_invoices/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Bright Cloud Studio's GAI Invoices
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/gai-invoices
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
