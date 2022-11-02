c1f0454ab70731856971625504bcd994f0f0cfd1
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-11-02 11:41:31 -0400

    Create modules.php

---
+++ system/modules/gai_invoices/languages/en/modules.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Modal Gallery
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/modal-gallery
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Front end modules */
+$GLOBALS['TL_LANG']['FMD']['mod_recent_invoices'] 	= array('Recent Invoices', 'Display recent invoices, select and modify them.');
