c3493a9408209855f8189e404d81e129a47c0e3e
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-11-21 12:35:55 -0500

    Update autoload.php

--- system/modules/gai_invoices/config/autoload.php
+++ system/modules/gai_invoices/config/autoload.php
@@ -15,6 +15,8 @@ ClassLoader::addClasses(array
 (
     // this is the hooks class
     'Bcs\Handler'                               => 'system/modules/gai_invoices/library/Bcs/Handler.php',
+    // Create Invoice
+    'Bcs\Module\ModCreateInvoice'              => 'system/modules/gai_invoices/library/Bcs/Module/ModCreateInvoice.php',
     // this is the front end module for displaying recent invoices for editing
     'Bcs\Module\ModRecentInvoices'              => 'system/modules/gai_invoices/library/Bcs/Module/ModRecentInvoices.php',
 ));
@@ -22,6 +24,11 @@ ClassLoader::addClasses(array
 /* Register the templates */
 TemplateLoader::addFiles(array
 (
+    // Create Invoices
+    'mod_create_invoice'            => 'system/modules/gai_invoices/templates/modules',
+    'item_work_assignment'          => 'system/modules/gai_invoices/templates/items',
+    
+    
     // this is the template for the recent invoices module
 	'mod_recent_invoices'                   => 'system/modules/gai_invoices/templates/modules',
     // this is the template for the selected recent invoice inside the recent invoices module
