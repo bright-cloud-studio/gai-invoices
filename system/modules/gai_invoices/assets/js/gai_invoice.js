4f4b0bdd78e44b5bf9e6887445f3e78581b13261
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-12-02 15:04:19 -0500

    Update gai_invoice.js

--- system/modules/gai_invoices/assets/js/gai_invoice.js
+++ system/modules/gai_invoices/assets/js/gai_invoice.js
@@ -1,38 +1,36 @@
-
-// when the page loads, check if there is anything in the cart. If there is update the cart number
-// A $( document ).ready() block.
+// when document has fully loaded
 $( document ).ready(function() {
   
   
   
-  // this is the call to save to cart
-function remove_from_cart(id){
+    // called when selecting a Work Assignment on the Create an Invoice page
+    function selectWorkAssignment(date, district, student){
 
-    // trigger this function when our form runs
-    $.ajax({
-        url:'/system/modules/panel_pricing_calculator/assets/php/action.remove.from.cart.endpoint.php',
-        type:'POST',
-        data:"entry_id="+id+"",
-        success:function(result){
-        	console.log("REMOVED SUCCESS");
-        	
-        	var ourItem = '#cart_item_' + id;
-        	console.log("SELECTOR: " + ourItem);
-        	$(ourItem).remove();
-        	
-        	if ($('#cart_contents').is(':empty')){
-        		$("#calc_cart_container").slideUp();
-			}
-        	
-        	$("#cart_total").html(result);
+        // trigger this function when our form runs
+        $.ajax({
+            url:'/system/modules/panel_pricing_calculator/assets/php/action.remove.from.cart.endpoint.php',
+            type:'POST',
+            data:"entry_id="+id+"",
+            success:function(result){
+                console.log("REMOVED SUCCESS");
 
-        },
-        error:function(result){
-			$("#dev_message").html("REMOVE FROM CART FAIL");
-        }
-    });
-	
-}
+                var ourItem = '#cart_item_' + id;
+                console.log("SELECTOR: " + ourItem);
+                $(ourItem).remove();
+
+                if ($('#cart_contents').is(':empty')){
+                $("#calc_cart_container").slideUp();
+                }
+
+                $("#cart_total").html(result);
+
+            },
+            error:function(result){
+                $("#dev_message").html("REMOVE FROM CART FAIL");
+            }
+        });
+
+    }
   
   
   
