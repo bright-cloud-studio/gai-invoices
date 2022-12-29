c19b434fa95da7b659fe4ae2a4d2d6c001802780
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-12-29 14:22:02 -0500

    Update ModWorkAssignmentHistory.php

--- system/modules/gai_invoices/library/Bcs/Module/ModWorkAssignmentHistory.php
+++ system/modules/gai_invoices/library/Bcs/Module/ModWorkAssignmentHistory.php
@@ -1,11 +1,11 @@
 <?php
 
 /**
- * Bright Cloud Studio's GAI Invoices
+ * Bright Cloud Studio's Modal Gallery
  *
- * Copyright (C) 2022-2023 Bright Cloud Studio
+ * Copyright (C) 2021 Bright Cloud Studio
  *
- * @package    bright-cloud-studio/gai-invoices
+ * @package    bright-cloud-studio/modal-gallery
  * @link       https://www.brightcloudstudio.com/
  * @license    http://opensource.org/licenses/lgpl-3.0.html
 **/
@@ -15,6 +15,8 @@ namespace Bcs\Module;
 
 use Google;
 
+
+ 
 class ModWorkAssignmentHistory extends \Contao\Module
 {
  
@@ -80,7 +82,7 @@ class ModWorkAssignmentHistory extends \Contao\Module
     protected function compile()
     {
         
-        /*
+        
         // get the user and build their name
         $objUser = \FrontendUser::getInstance();
         $user = $objUser->firstname . " " . $objUser->lastname;
@@ -89,13 +91,12 @@ class ModWorkAssignmentHistory extends \Contao\Module
         $spreadsheet = $this->$service->spreadsheets->get(ModCreateInvoice::$spreadsheetId);
         
         // get all of our unarchived Transactions
-        $range = 'Work Assignment';
+        $range = 'Invoices - Psy';
         $response = $this->$service->spreadsheets_values->get(ModCreateInvoice::$spreadsheetId, $range);
         $values = $response->getValues();
         
         // an array to store this users entries
-        $entryList = array();
-        $entryForm = array();
+        $entryHistory = array();
         $objUser = \FrontendUser::getInstance();
         
         $entry_id = 0;
@@ -106,52 +107,28 @@ class ModWorkAssignmentHistory extends \Contao\Module
                 
                 if($user == $entry[3]) {
                     $arrData = array();
-                    $arrData['date']                = $entry[0];
-                    $arrData['30_days']             = $entry[1];
-                    $arrData['45_days']             = $entry[2];
-                    $arrData['psychologist']        = $entry[3];
-                    $arrData['district']            = $entry[4];
-                    $arrData['student_name']        = $entry[5];
-                    $arrData['date_of_birth']       = $entry[6];
-                    $arrData['grade']               = $entry[7];
-                    $arrData['lasid']               = $entry[8];
-                    $arrData['sasid']               = $entry[9];
-                    $arrData['initial']             = $entry[10];
-                    $arrData['type_of_testing']     = $entry[11];
-                    $arrData['testing_date']        = $entry[12];
-                    $arrData['meeting_required']    = $entry[13];
-                    $arrData['meeting_date']        = $entry[14];
-                    $arrData['parent_info']         = $entry[15];
-                    $arrData['teacher_info']        = $entry[16];
-                    $arrData['team_chair']          = $entry[17];
-                    $arrData['email']               = $entry[18];
-                    $arrData['report_submitted']    = $entry[19];
-                    $arrData['invoiced_to_gai']     = $entry[20];
-                    $arrData['district_invoice']    = $entry[21];
-                    $arrData['notes']               = $entry[22];
+                    $arrData['billing_year']    = $entry[0];
+                    $arrData['billing_month']   = $entry[1];
+                    $arrData['invoice_number']  = $entry[2];
+                    $arrData['psychologist']    = $entry[3];
+                    $arrData['date_issued']     = $entry[4];
+                    $arrData['date_due']        = $entry[5];
+                    $arrData['invoice_link']    = $entry[6];
                     
                     // Generate as "List"
-                    $strListTemplate = ($this->entry_customItemTpl != '' ? $this->entry_customItemTpl : 'work_assignment_list');
+                    $strListTemplate = ($this->entry_customItemTpl != '' ? $this->entry_customItemTpl : 'work_assignment_history');
                     $objListTemplate = new \FrontendTemplate($strListTemplate);
                     $objListTemplate->setData($arrData);
-                    $entryList[$entry_id] = $objListTemplate->parse();
-                    
-                    // Generate as "Form"
-                    $strFormTemplate = ($this->entry_customItemTpl != '' ? $this->entry_customItemTpl : 'work_assignment_form');
-                    $objFormTemplate = new \FrontendTemplate($strFormTemplate);
-                    $objFormTemplate->setData($arrData);
-                    $entryForm[$entry_id] = $objFormTemplate->parse();
+                    $entryHistory[$entry_id] = $objListTemplate->parse();
                 }
-                
+
             }
             
             $entry_id++;
         }
         
         // set this users entries to the template
-        $this->Template->workAssignmentList = $entryList;
-        $this->Template->workAssignmentForm = $entryForm;
+        $this->Template->workAssignmentHistory = $entryHistory;
         
-        */
 	}
 } 
