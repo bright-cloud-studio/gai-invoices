25d6d647d9778b15a65087e22e27aeeb84d09f36
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-01-31 08:36:31 -0500

    Create action.get.schools.php

---
+++ system/modules/gai_invoices/assets/php/action.get.schools.php
@@ -0,0 +1,37 @@
+<?php
+    
+    // Start PHP session and include Composer, which also brings in our Google Sheets PHP stuffs
+	session_start();
+	require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
+    
+
+    // Create a client connection to Google
+    $client = new Google\Client();
+    $client->setAuthConfig($_SERVER['DOCUMENT_ROOT'] . '/key.json');
+    $client->addScope(Google\Service\Sheets::SPREADSHEETS);
+    $service = new \Google_Service_Sheets($client);
+    $spreadsheetId = '1PEJN5ZGlzooQrtIEdeo4_nZH73W0aJTUbRIoibzl3Lo';
+    
+   
+   
+    $range = 'Psychologists';
+    $response = $service->spreadsheets_values->get($spreadsheetId, $range);
+    $values = $response->getValues();
+   
+    $names = array();
+   
+   $names_index = 0;
+    foreach($values as $entry) {
+        
+
+        
+        if($names_index >= 1) {
+            array_push($names, $entry['1']);
+        }
+        
+        
+        $names_index++;
+        
+    }
+    
+    echo json_encode($names);
