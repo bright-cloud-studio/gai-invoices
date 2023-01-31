9e0f91a8c15c66620800175b89b199ba204dc830
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-01-31 08:58:58 -0500

    Update tl_member.php

--- system/modules/gai_invoices/dca/tl_member.php
+++ system/modules/gai_invoices/dca/tl_member.php
@@ -11,7 +11,7 @@
 
  /* Extend the tl_user palettes */
 foreach ($GLOBALS['TL_DCA']['tl_member']['palettes'] as $k => $v) {
-    $GLOBALS['TL_DCA']['tl_member']['palettes'][$k] = str_replace('groups;', 'groups;{meetings_legend},meeting_option;', $v);
+    $GLOBALS['TL_DCA']['tl_member']['palettes'][$k] = str_replace('groups;', 'groups;{meetings_legend},meeting_options;', $v);
 }
     
 /* Add fields to tl_user */
