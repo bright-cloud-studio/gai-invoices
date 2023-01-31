5d704b8dcca8ed3d4fcbdc4b7e1883f13abcc9bb
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-01-31 08:51:39 -0500

    Create tl_member.php

---
+++ system/modules/gai_invoices/languages/en/tl_member.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Add User Fields
+ *
+ * Copyright (C) 2021 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/add-user-fields
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_member']['meeting_options']          = array('Meeting Options', 'Select which meeting options are available to this Member');
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_member']['meeting_legend'] = 'Dashboard - Meeting Options';
