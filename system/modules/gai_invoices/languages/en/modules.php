9b1f5fa86d3fbbb9da44994dc153c95427375783
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-02-01 13:28:49 -0500

    Update modules.php

--- system/modules/gai_invoices/languages/en/modules.php
+++ system/modules/gai_invoices/languages/en/modules.php
@@ -15,5 +15,6 @@ $GLOBALS['TL_LANG']['MOD']['gai_invoices']                  = array('GAI Invoice
 /* Front end modules */
 $GLOBALS['TL_LANG']['FMD']['mod_create_invoice']            = array('Create Invoice', 'Displays list of Work Assignments then creates Transactions for them');
 $GLOBALS['TL_LANG']['FMD']['mod_add_meetings']              = array('Add Meetings', 'Manually add meeting transactions without a Work Assignment');
+$GLOBALS['TL_LANG']['FMD']['mod_misc_billing']              = array('Miscellaneous Billing', 'Manually add miscellaneous billing');
 $GLOBALS['TL_LANG']['FMD']['mod_work_assignment_history'] 	= array('Work Assignment History', 'Displays history of processed Work Assignments');
 $GLOBALS['TL_LANG']['FMD']['mod_transaction_review']        = array('Transaction Review', 'List and review transactions submitted in the last month');
