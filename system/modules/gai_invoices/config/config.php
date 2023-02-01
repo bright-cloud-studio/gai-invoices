e1efc4818213ff0665e4011f13864004e7b1dccf
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-02-01 13:27:28 -0500

    Update config.php

--- system/modules/gai_invoices/config/config.php
+++ system/modules/gai_invoices/config/config.php
@@ -15,6 +15,7 @@
 // Create Invoice - List's a psychologists entries in the Work Assignment sheet
 $GLOBALS['FE_MOD']['gai_invoices']['mod_create_invoice']            = 'Bcs\Module\ModCreateInvoice';
 $GLOBALS['FE_MOD']['gai_invoices']['mod_add_meetings']              = 'Bcs\Module\ModAddMeetings';
+$GLOBALS['FE_MOD']['gai_invoices']['mod_misc_billing']              = 'Bcs\Module\ModMiscBilling';
 $GLOBALS['FE_MOD']['gai_invoices']['mod_work_assignment_history'] 	= 'Bcs\Module\ModWorkAssignmentHistory';
 $GLOBALS['FE_MOD']['gai_invoices']['mod_transaction_review'] 	    = 'Bcs\Module\ModTransactionReview';
 
