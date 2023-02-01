efe6990b7c8eebecc75001181541168bb8c2e224
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-02-01 13:22:00 -0500

    Update action.get.districts.php

--- system/modules/gai_invoices/assets/php/action.get.districts.php
+++ system/modules/gai_invoices/assets/php/action.get.districts.php
@@ -4,6 +4,7 @@
 	session_start();
 	require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
     
+
     // Create a client connection to Google
     $client = new Google\Client();
     $client->setAuthConfig($_SERVER['DOCUMENT_ROOT'] . '/key.json');
@@ -11,6 +12,8 @@
     $service = new \Google_Service_Sheets($client);
     $spreadsheetId = '1PEJN5ZGlzooQrtIEdeo4_nZH73W0aJTUbRIoibzl3Lo';
     
+   
+   
     $range = 'Schools';
     $response = $service->spreadsheets_values->get($spreadsheetId, $range);
     $values = $response->getValues();
@@ -20,6 +23,8 @@
     $names_index = 0;
     foreach($values as $entry) {
         
+
+        
         if($names_index >= 1) {
             if($entry['2'] != null) {
                 if(!in_array($entry['2'], $names)) {
@@ -27,6 +32,7 @@
                 }
             }
         }
+        
         
         $names_index++;
         
