530690afc186374ca568eb205eebb919c8ba4061
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-03-03 14:56:50 -0500

    Create action.send.invoice.emails.php

---
+++ system/modules/gai_invoices/assets/php/action.send.invoice.emails.php
@@ -0,0 +1,34 @@
+<?php
+    
+    // Start PHP session and include Composer, which also brings in our Google Sheets PHP stuffs
+	session_start();
+	require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
+    
+    // Store the passed form values
+    $vars = $_POST;
+
+    // Create a client connection to Google
+    $client = new Google\Client();
+    $client->setAuthConfig($_SERVER['DOCUMENT_ROOT'] . '/key.json');
+    $client->addScope(Google\Service\Sheets::SPREADSHEETS);
+    $service = new \Google_Service_Sheets($client);
+    $spreadsheetId = '1PEJN5ZGlzooQrtIEdeo4_nZH73W0aJTUbRIoibzl3Lo';
+    
+    // Mark this Work Assignment as Processed
+    $updateRow = [
+       "1",
+    ];
+    $rows = [$updateRow];
+    $valueRange = new \Google_Service_Sheets_ValueRange();
+    $valueRange->setValues($rows);
+    
+    $options = ['valueInputOption' => 'USER_ENTERED'];
+    
+    // loop for the total transactions amount, update the "Reviewed" column
+    for ($x = 1; $x <= $vars['trans_total']; $x++) {
+        $range = 'Transactions!P' . $vars['row_id_'.$x];
+        $service->spreadsheets_values->update($spreadsheetId, $range, $valueRange, $options);
+    }
+
+    // display some text to return back to the ajax call
+    echo "success";
