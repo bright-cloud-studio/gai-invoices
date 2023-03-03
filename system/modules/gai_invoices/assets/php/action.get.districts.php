3b641a6b11aa86d290f4c0509de621f3777882e3
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-03-03 14:44:59 -0500

    Update action.get.districts.php

--- system/modules/gai_invoices/assets/php/action.get.districts.php
+++ system/modules/gai_invoices/assets/php/action.get.districts.php
@@ -4,7 +4,6 @@
 	session_start();
 	require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
     
-
     // Create a client connection to Google
     $client = new Google\Client();
     $client->setAuthConfig($_SERVER['DOCUMENT_ROOT'] . '/key.json');
@@ -12,30 +11,33 @@
     $service = new \Google_Service_Sheets($client);
     $spreadsheetId = '1PEJN5ZGlzooQrtIEdeo4_nZH73W0aJTUbRIoibzl3Lo';
     
-   
-   
+   // Get all of the School entries on the master list
     $range = 'Schools';
     $response = $service->spreadsheets_values->get($spreadsheetId, $range);
     $values = $response->getValues();
-   
+    
+    // Store our names in this array
     $names = array();
-   
+    
+    // Count our index each tick of the foreach loop
     $names_index = 0;
     foreach($values as $entry) {
         
-
-        
+        // Only do stuff after our first run, as the first run is just the table header information
         if($names_index >= 1) {
+            // if our value isnt empty
             if($entry['2'] != null) {
+                // if this name isn't already in the array
                 if(!in_array($entry['2'], $names)) {
+                    // add our value to our Names array
                     array_push($names, $entry['2']);
                 }
             }
         }
-        
-        
+        // tick up our index counter
         $names_index++;
-        
+
     }
     
+    // encode our PHP array into a JSON format and send that puppy back to the AJAX call
     echo json_encode($names);
