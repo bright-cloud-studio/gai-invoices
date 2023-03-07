017703ff32849577c1aff22c407b7303d6751020
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-03-07 13:49:29 -0500

    Update config.php

--- system/modules/gai_invoices/config/config.php
+++ system/modules/gai_invoices/config/config.php
@@ -20,3 +20,18 @@ $GLOBALS['FE_MOD']['gai_invoices']['mod_work_assignment_history'] 	= 'Bcs\Module
 $GLOBALS['FE_MOD']['gai_invoices']['mod_transaction_review'] 	    = 'Bcs\Module\ModTransactionReview';
 $GLOBALS['FE_MOD']['gai_invoices']['mod_admin_review'] 	            = 'Bcs\Module\ModAdminReview';
 $GLOBALS['FE_MOD']['gai_invoices']['mod_send_invoice_emails'] 	    = 'Bcs\Module\ModSendInvoiceEmails';
+
+/** Add new notification type */
+$GLOBALS['NOTIFICATION_CENTER']['NOTIFICATION_TYPE']['gai_invoices']['send_email'] = array
+(
+     'recipients'           => array('admin_email', 'recipient_email'),
+     'email_subject'        => array('admin_email', 'news_archive_*', 'news_*', 'news_text', 'news_url', 'recipient_email'),
+     'email_text'           => array('admin_email', 'news_archive_*', 'news_*', 'news_text', 'news_url', 'recipient_email'),
+     'email_html'           => array('admin_email', 'news_archive_*', 'news_*', 'news_text', 'news_url', 'recipient_email'),
+     'file_name'            => array('admin_email', 'news_archive_*', 'news_*', 'news_text', 'news_url', 'recipient_email'),
+     'file_content'         => array('admin_email', 'news_archive_*', 'news_*', 'news_text', 'news_url', 'recipient_email'),
+     'email_recipient_cc'   => array('admin_email', 'news_archive_*', 'news_*', 'news_text', 'news_url', 'recipient_email'),
+     'email_recipient_bcc'  => array('admin_email', 'news_archive_*', 'news_*', 'news_text', 'news_url', 'recipient_email'),
+     'email_replyTo'        => array('admin_email', 'news_archive_*', 'news_*', 'news_text', 'news_url', 'recipient_email'),
+     'attachment_tokens'    => array(),
+);
