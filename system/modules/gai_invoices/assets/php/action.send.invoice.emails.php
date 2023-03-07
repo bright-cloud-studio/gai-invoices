f3c37f3dba23b008ea70a66279154c8083454136
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-03-07 11:39:02 -0500

    Update action.send.invoice.emails.php

--- system/modules/gai_invoices/assets/php/action.send.invoice.emails.php
+++ system/modules/gai_invoices/assets/php/action.send.invoice.emails.php
@@ -6,7 +6,7 @@
     
     // Store the passed form values
     $vars = $_POST;
-
+    
     // Create a client connection to Google
     $client = new Google\Client();
     $client->setAuthConfig($_SERVER['DOCUMENT_ROOT'] . '/key.json');
@@ -16,19 +16,83 @@
     
     // Mark this Work Assignment as Processed
     $updateRow = [
-       "1",
+       "Yes",
     ];
     $rows = [$updateRow];
     $valueRange = new \Google_Service_Sheets_ValueRange();
     $valueRange->setValues($rows);
-    
     $options = ['valueInputOption' => 'USER_ENTERED'];
     
-    // loop for the total transactions amount, update the "Reviewed" column
-    for ($x = 1; $x <= $vars['trans_total']; $x++) {
-        $range = 'Transactions!P' . $vars['row_id_'.$x];
-        $service->spreadsheets_values->update($spreadsheetId, $range, $valueRange, $options);
+    
+    // Get date in desired format "06_19_1987"
+    $date = date("m_d_y");
+    // Write to our file, as email logging is complicated
+    $myfile = fopen("email_".$date.".txt", "w") or die("Unable to open file!");
+    
+    
+    // loop through psys and send emails
+    for ($x = 1; $x <= $vars['psy_total']; $x++) {
+        
+        if(!empty($vars['send_psy_'.$x])) {
+            $to = $vars['email_psy_'.$x];
+            //$to = "mark@brightcloudstudio.com";
+            $subject = "Global Assessments, Inc. - A new invoice is ready for you";
+            $txt = "Global Assessments, Inc. has a new invoice for you. Click the link below to view it: \n\n" . $vars['url_psy_'.$x];
+            $headers = "From: ed@globalassessmentsinc.com" . "\r\n" ."CC: ed@globalassessmentsinc.com";
+            
+            $success = mail($to,$subject,$txt,$headers);
+            if($success){
+               
+               // mail successful, update Sent on Sheets
+               $range = 'Invoices - Psy!H' . $vars['row_id_psy_'.$x];
+               $service->spreadsheets_values->update($spreadsheetId, $range, $valueRange, $options);
+               
+                fwrite($myfile, "Email Sent - " . $to . ", " . $headers . "\r\n");
+
+            } else {
+                error_log("Failure Sending Psy Mail - " . $vars['email_psy_'.$x]);
+                fwrite($myfile, "Failure - " . $to . ", " . $headers . "\r\n");
+            }
+
+        }
+        
+    }
+    
+    // loop through schools and send emails
+    for ($x = 1; $x <= $vars['school_total']; $x++) {
+        
+        if(!empty($vars['send_school_'.$x])) {
+            $to = $vars['email_school_'.$x];
+            //$to = "mark@brightcloudstudio.com";
+            $subject = "Global Assessments, Inc. - A new invoice is ready for you";
+            $txt = "Global Assessments, Inc. has a new invoice for you. Click the link below to view it: \n\n" . $vars['url_school_'.$x];
+            
+            $headers = '';
+            
+            if($vars['cc_school_'.$x] != '')
+                $headers = "From: ed@globalassessmentsinc.com" . "\r\n" ."CC: ". $vars['cc_school_'.$x] . ", ed@globalassessmentsinc.com";
+            else
+                $headers = "From: ed@globalassessmentsinc.com" . "\r\n" ."CC: ed@globalassessmentsinc.com";
+            
+            $success = mail($to,$subject,$txt,$headers);
+            if($success){
+               
+               // mail successful, update Sent on Sheets
+               $range = 'Invoices - School!I' . $vars['row_id_school_'.$x];
+               $service->spreadsheets_values->update($spreadsheetId, $range, $valueRange, $options);
+               
+               fwrite($myfile, "Email Sent - " . $to . ", " . $headers . "\r\n");
+               
+            } else {
+                error_log("Failure Sending School Mail - " . $vars['email_school_'.$x]);
+                fwrite($myfile, "Failure - " . $to . ", " . $headers . "\r\n");
+            }
+            
+        }
+        
     }
 
+    fclose($myfile);
+    
     // display some text to return back to the ajax call
     echo "success";
