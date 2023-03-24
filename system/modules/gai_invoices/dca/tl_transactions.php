1d8781b84ce7506d49d63c7b6a82617fe0d053ce
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-03-24 10:16:44 -0400

    Update tl_transactions.php

--- system/modules/gai_invoices/dca/tl_transactions.php
+++ system/modules/gai_invoices/dca/tl_transactions.php
@@ -36,7 +36,8 @@ $GLOBALS['TL_DCA']['tl_transactions'] = array
         'sorting' => array
         (
             'mode'                    => 0,
-            'panelLayout'             => 'filter;search,limit'
+            'panelLayout'             => 'filter;search,limit',
+            'fields'                  => array('approved','country'),
         ),
         'label' => array
         (
@@ -57,34 +58,34 @@ $GLOBALS['TL_DCA']['tl_transactions'] = array
         (
             'edit' => array
             (
-                'label'               => &$GLOBALS['TL_LANG']['tl_price_chart_large']['edit'],
+                'label'               => &$GLOBALS['TL_LANG']['tl_transactions']['edit'],
                 'href'                => 'act=edit',
                 'icon'                => 'edit.gif'
             ),
 			
             'copy' => array
             (
-                'label'               => &$GLOBALS['TL_LANG']['tl_price_chart_large']['copy'],
+                'label'               => &$GLOBALS['TL_LANG']['tl_transactions']['copy'],
                 'href'                => 'act=copy',
                 'icon'                => 'copy.gif'
             ),
             'delete' => array
             (
-                'label'               => &$GLOBALS['TL_LANG']['tl_price_chart_large']['delete'],
+                'label'               => &$GLOBALS['TL_LANG']['tl_transactions']['delete'],
                 'href'                => 'act=delete',
                 'icon'                => 'delete.svg',
                 'attributes'          => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\'))return false;Backend.getScrollOffset()"'
             ),
             'toggle' => array
 			(
-				'label'               => &$GLOBALS['TL_LANG']['tl_price_chart_large']['toggle'],
+				'label'               => &$GLOBALS['TL_LANG']['tl_transactions']['toggle'],
 				'icon'                => 'visible.gif',
 				'attributes'          => 'onclick="Backend.getScrollOffset();return AjaxRequest.toggleVisibility(this,%s)"',
-				'button_callback'     => array('Bcs\Backend\PriceChartLargeBackend', 'toggleIcon')
+				'button_callback'     => array('Bcs\Backend\TransactionsBackend', 'toggleIcon')
 			),
             'show' => array
             (
-                'label'               => &$GLOBALS['TL_LANG']['tl_price_chart_large']['show'],
+                'label'               => &$GLOBALS['TL_LANG']['tl_transactions']['show'],
                 'href'                => 'act=show',
                 'icon'                => 'show.gif'
             )
@@ -108,40 +109,193 @@ $GLOBALS['TL_DCA']['tl_transactions'] = array
         (
 		'sql'                     	=> "int(10) unsigned NOT NULL default '0'"
         ),
-	'sorting' => array
-	(
-		'sql'                    	=> "int(10) unsigned NOT NULL default '0'"
-	),
-	'alias' => array
-	(
-		'label'                   => &$GLOBALS['TL_LANG']['tl_price_chart_large']['alias'],
-		'exclude'                 => true,
-		'inputType'               => 'text',
-		'search'                  => true,
-		'eval'                    => array('unique'=>true, 'rgxp'=>'alias', 'doNotCopy'=>true, 'maxlength'=>128, 'tl_class'=>'w50'),
-		'save_callback' => array
-		(
-			array('Bcs\Backend\PriceChartLargeBackend', 'generateAlias')
-		),
-		'sql'                     => "varchar(128) COLLATE utf8_bin NOT NULL default ''"
+        'sorting' => array
+        (
+            'sql'                    	=> "int(10) unsigned NOT NULL default '0'"
+        ),
+        'alias' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_transactions']['alias'],
+            'exclude'                 => true,
+            'inputType'               => 'text',
+            'search'                  => true,
+            'eval'                    => array('unique'=>true, 'rgxp'=>'alias', 'doNotCopy'=>true, 'maxlength'=>128, 'tl_class'=>'w50'),
+            'save_callback' => array
+            (
+                array('Bcs\Backend\TransactionsBackend', 'generateAlias')
+            ),
+            'sql'                     => "varchar(128) COLLATE utf8_bin NOT NULL default ''"
 
-	),
-	'width' => array
-	(
-		'label'                   => &$GLOBALS['TL_LANG']['tl_price_chart_large']['width'],
-		'inputType'               => 'text',
-		'default'		  => '',
-		'search'                  => true,
-		'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
-		'sql'                     => "varchar(255) NOT NULL default ''"
-	),
-	'published' => array
-	(
-		'exclude'                 => true,
-		'label'                   => &$GLOBALS['TL_LANG']['tl_price_chart_large']['published'],
-		'inputType'               => 'checkbox',
-		'eval'                    => array('submitOnChange'=>true, 'doNotCopy'=>true),
-		'sql'                     => "char(1) NOT NULL default ''"
-	)		
+        ),
+        'date' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_transactions']['date'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'psychologist' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_transactions']['psychologist'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'district' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_transactions']['district'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'school' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_transactions']['school'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'student_name' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_transactions']['student_name'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'service_provided' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_transactions']['service_provided'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'price' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_transactions']['price'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'lasid' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_transactions']['lasid'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'sasid' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_transactions']['sasid'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'meeting_date' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_transactions']['meeting_date'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'meeting_start' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_transactions']['meeting_start'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'meeting_end' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_transactions']['meeting_end'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'meeting_duration' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_transactions']['meeting_duration'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'notes' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_transactions']['notes'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'reviewed' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_transactions']['reviewed'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'deleted' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_transactions']['deleted'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'misc_billing' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_transactions']['misc_billing'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'sheet_row' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_transactions']['sheet_row'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'published' => array
+        (
+            'exclude'                 => true,
+            'label'                   => &$GLOBALS['TL_LANG']['tl_transactions']['published'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('submitOnChange'=>true, 'doNotCopy'=>true),
+            'sql'                     => "char(1) NOT NULL default ''"
+        )		
     )
 );
