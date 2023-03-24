dbb9bbd50305b9dca9f75f78679f30ff676b83ee
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-03-24 09:21:05 -0400

    Update action.process.work.assignment.php

--- system/modules/gai_invoices/assets/php/action.process.work.assignment.php
+++ system/modules/gai_invoices/assets/php/action.process.work.assignment.php
@@ -6,6 +6,24 @@
     
     // Store the passed form values
     $vars = $_POST;
+    
+    // Clean up our psychologist name so it can be used as a filename
+	$cleanName = str_replace(' ', '_', $vars['psychologist']);
+	$cleanName = str_replace('.', '', $cleanName);
+	$cleanName = preg_replace('/[^a-zA-Z0-9_.]/', '_', strtolower($cleanName));
+    
+    
+    
+    // create a file with the name "psy_datetime.txt" to log our transaction data
+    $myfile = fopen("transaction_logs/".$cleanName."_".date('m_d_Y_hia').".txt", "w") or die("Unable to open file!");
+    // loop through our $vars and write the key => value to our created file
+    foreach($vars as $key => $var) {
+        fwrite($myfile, "Key: " . $key . "  | Value: " . $var . "\n");
+    }
+    // were done logging, close the file we just created
+    fclose($myfile);
+    
+    
 
     // Create a client connection to Google
     $client = new Google\Client();
@@ -13,7 +31,6 @@
     $client->addScope(Google\Service\Sheets::SPREADSHEETS);
     $service = new \Google_Service_Sheets($client);
     $spreadsheetId = '1PEJN5ZGlzooQrtIEdeo4_nZH73W0aJTUbRIoibzl3Lo';
-    
     
     $meeting_duration = 0;
     
@@ -24,9 +41,7 @@
         // remove decimal and trailing numbers
         $price = floor($price);
     }
-    
-    
-    
+
     // For each transaction fieldset on the form, create a new row in the Transactions table on Sheets
     for ($x = 1; $x <= $vars["transactions"]; $x++) {
         
@@ -53,7 +68,11 @@
                 $vars['meeting_start'],
                 $vars['meeting_end'],
                 $meeting_duration,
-                $vars['notes']
+                $vars['notes'],
+                '', // Reviewed
+                '', // Deleted
+                '', // Misc Billing
+                $vars['sheet_row'] // Work Assignment ID
             ];
             
             $rows = [$newRow];
@@ -91,7 +110,11 @@
                 $vars['meeting_start_' . $x],
                 $vars['meeting_end_' . $x],
                 $meeting_duration,
-                $vars['notes_' . $x]
+                $vars['notes_' . $x],
+                '', // Reviewed
+                '', // Deleted
+                '', // Misc. Billing
+                $vars['sheet_row'] // Work Assignment ID
             ];
 
             $rows = [$newRow];
@@ -119,8 +142,6 @@
     $valueRange = new \Google_Service_Sheets_ValueRange();
     $valueRange->setValues($rows);
     
-    //$range = 'Work Assignment!Y' . $vars['sheet_row']; // where the replacement will start, here, first column and second line
-    
     $range = 'Fall!AA' . $vars['sheet_row'];
     $spreadsheetId = '1erZUWlCgpWd67E1PIwwKNCYT0yCm2QiV2DL28VA8oVU';
     
@@ -130,11 +151,9 @@
     if($vars['psychologist'] == $vars['shared_4']) { $range = 'Fall!AI' . $vars['sheet_row']; }
     if($vars['psychologist'] == $vars['shared_5']) { $range = 'Fall!AK' . $vars['sheet_row']; }
     
-    
     $options = ['valueInputOption' => 'USER_ENTERED'];
     $service->spreadsheets_values->update($spreadsheetId, $range, $valueRange, $options);
     
-
     // display some text to return back to the ajax call
     echo "success";
     
