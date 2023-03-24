1ecda87fc29c57a0dc62bd34ccd3ee3c742cee69
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-03-24 10:33:42 -0400

    Create tl_transactions.php

---
+++ system/modules/gai_invoices/languages/en/tl_transactions.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Bright Cloud Studio's GAI Invoices
+ *
+ * Copyright (C) 2022-2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/gai-invoices
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+/* System Buttons */
+$GLOBALS['TL_LANG']['tl_transactions']['new']                        = array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_transactions']['show']                       = array('Record details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_transactions']['edit']                       = array('Edit record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_transactions']['copy']                       = array('Copy record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_transactions']['delete']                     = array('Delete record', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_transactions']['toggle']                     = array('Toggle record', 'Toggle record ID %s');
+
+/* System Fields */
+$GLOBALS['TL_LANG']['tl_transactions']['publish_legend']             = 'Publish';
+$GLOBALS['TL_LANG']['tl_transactions']['alias']                      = array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_transactions']['published']                  = array('Published', 'Show this record on the front end.');
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_transactions']['date']                       = array('Date', 'Today\'s Date');
+$GLOBALS['TL_LANG']['tl_transactions']['psychologist']               = array('Psychologist', 'Psycholigist this transactions is for');
+$GLOBALS['TL_LANG']['tl_transactions']['district']                   = array('District', 'District the school is in');
+$GLOBALS['TL_LANG']['tl_transactions']['school']                     = array('School', 'The name of the school');
+$GLOBALS['TL_LANG']['tl_transactions']['student_name']               = array('Student Name', 'The initials of the student');
+$GLOBALS['TL_LANG']['tl_transactions']['service_provided']           = array('Service Provided', 'The ID of the service being provided');
+$GLOBALS['TL_LANG']['tl_transactions']['price']                      = array('Price', 'Price for this transaction');
+$GLOBALS['TL_LANG']['tl_transactions']['lasid']                      = array('LASID', 'The LASID assigned to the student');
+$GLOBALS['TL_LANG']['tl_transactions']['sasid']                      = array('SASID', 'The SASID assigned to the student');
+$GLOBALS['TL_LANG']['tl_transactions']['meeting_date']               = array('Meeting Date', 'The date the meeting occured');
+$GLOBALS['TL_LANG']['tl_transactions']['meeting_start']              = array('Meeting Start', 'The time the meeting started');
+$GLOBALS['TL_LANG']['tl_transactions']['meeting_end']                = array('Meeting End', 'The time the meeting ended');
+$GLOBALS['TL_LANG']['tl_transactions']['meeting_duration']           = array('Meeting Duration', 'The amount of minutes the meeting lasted for');
+$GLOBALS['TL_LANG']['tl_transactions']['notes']                      = array('Notes', 'Notes added by the psychologist');
+$GLOBALS['TL_LANG']['tl_transactions']['reviewed']                   = array('Reviewed', 'INTERAL USE ONLY');
+$GLOBALS['TL_LANG']['tl_transactions']['deleted']                    = array('Deleted', 'INTERNAL USE ONLY');
+$GLOBALS['TL_LANG']['tl_transactions']['misc_billing']               = array('Misc. Billing', 'INTERNAL USE ONLY');
+$GLOBALS['TL_LANG']['tl_transactions']['sheet_row']                  = array('Sheet Row', 'INTERNAL USE ONLY');
