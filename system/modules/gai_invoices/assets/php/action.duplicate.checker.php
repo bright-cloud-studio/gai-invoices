e095e7fb08cf1a9e6919073eb18ec914cb01ff61
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-03-27 13:36:04 -0400

    Create action.duplicate.checker.php

---
+++ system/modules/gai_invoices/assets/php/action.duplicate.checker.php
@@ -0,0 +1,15 @@
+<?php
+    
+    // Start PHP session and include Composer, which also brings in our Google Sheets PHP stuffs
+	session_start();
+	require_once $_SERVER['DOCUMENT_ROOT'] . '/../vendor/autoload.php';
+	
+	
+	
+	// connect to our DB
+	return "2";
+	
+	// check if the attempted transaction already exists in the database or not
+	
+	
+	
