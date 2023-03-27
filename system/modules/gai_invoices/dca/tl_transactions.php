2c96a3a88b131cd1f749a4f295d4ab798a221720
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-03-27 10:20:24 -0400

    Update tl_transactions.php

--- system/modules/gai_invoices/dca/tl_transactions.php
+++ system/modules/gai_invoices/dca/tl_transactions.php
@@ -37,7 +37,7 @@ $GLOBALS['TL_DCA']['tl_transactions'] = array
         (
             // Mode 2 - Records are sotrted by a switchable field
             // Flag 2 - Sort descending
-            'mode'                    => 0,
+            'mode'                    => 1,
             'panelLayout'             => 'sort,filter;search,limit',
             'fields'                  => array('date','psychologist'),
         ),
