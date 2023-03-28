89c3b1ff904495808a688228cf109b5637f25b98
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-03-28 11:24:50 -0400

    Update action.delete.transaction.php

--- system/modules/gai_invoices/assets/php/action.delete.transaction.php
+++ system/modules/gai_invoices/assets/php/action.delete.transaction.php
@@ -8,7 +8,7 @@
     $vars = $_POST;
     
     // create a file with the name "psy_datetime.txt" to log our transaction data
-    $myfile = fopen($_SERVER['DOCUMENT_ROOT'] . '/../delete_transaction/'.date('m_d_Y_hia').".txt", "w") or die("Unable to open file!");
+    $myfile = fopen($_SERVER['DOCUMENT_ROOT'] . '/../delete_transaction/'.$vars['psychologist']."_".date('m_d_Y_hia').".txt", "w") or die("Unable to open file!");
     // loop through our $vars and write the key => value to our created file
     foreach($vars as $key => $var) {
         fwrite($myfile, "Key: " . $key . "  | Value: " . $var . "\n");
@@ -59,6 +59,17 @@
         $service->spreadsheets_values->update($spreadsheetId, $range, $valueRange, $options);
         
     }
+    
+    
+    
+    // Connect to DB
+    $dbh = new mysqli("localhost", "globalassinc_user", "Z2rc^wQ}98TS9mtl5y", "globalassinc_contao_4_13");
+    if ($dbh->connect_error) {
+        die("Connection failed: " . $dbh->connect_error);
+    }
+    // insert into the tl_transactions table
+    $query = "UPDATE tl_transactions SET deleted='1' WHERE psychologist='".$vars['psychologist']."' AND district='".$vars['district']."' AND school='".$vars['school']."' AND student_name='".$vars['student']."' AND service_provided='".$vars['service']."' AND price='".$vars['price']."' AND meeting_date='".$vars['meeting_date']."' AND meeting_start='".$vars['meeting_start']."' AND meeting_end='".$vars['meeting_end']."'";
+    $result = $dbh->query($query);
    
     // display some text to return back to the ajax call
     echo "success";
