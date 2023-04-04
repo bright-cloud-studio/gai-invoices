a5c4c6ae72f38de3e1fdc98e3b0eaa6fdf0e6b4b
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-04-04 13:36:24 -0400

    Create sendInvoiceEmails.php

---
+++ system/modules/gai_invoices/library/Bcs/Frontend/sendInvoiceEmails.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Bcs\Frontend;
+
+use Contao\System;
+
+
+class SendInvoiceEmails extends Contao_Frontend {
+
+    protected $store_id = 0;
+
+    protected $strCookie = 'FE_USER_AUTH';
+
+    public function initializeApi() {
+        if (substr(\Environment::get('request'), 0, 4) == "api/") {
+
+        }
+    }
+
+}
