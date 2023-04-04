f417ed0a0d56ed807d56aa51338bf9f3e3a9a440
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-04-04 13:32:24 -0400

    Update config.php

--- system/modules/gai_invoices/config/config.php
+++ system/modules/gai_invoices/config/config.php
@@ -50,4 +50,4 @@ $GLOBALS['BE_MOD']['content']['transactions'] = array(
 $GLOBALS['TL_MODELS']['tl_transactions'] = 'Bcs\Model\Transactions';
 
 /* Hooks */
-$GLOBALS['TL_HOOKS']['initializeSystem'][] 	= array('Bcs\Module\ModSendInvoiceEmails', 'sendInvoiceEmails');
+//$GLOBALS['TL_HOOKS']['initializeSystem'][] 	= array('Bcs\Module\ModSendInvoiceEmails', 'sendInvoiceEmails');
