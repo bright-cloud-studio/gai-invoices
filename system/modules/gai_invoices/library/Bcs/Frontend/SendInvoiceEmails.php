01728a820a4635bf0faed0b21333dc47e24597f9
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-04-04 13:38:14 -0400

    Update SendInvoiceEmails.php

--- system/modules/gai_invoices/library/Bcs/Frontend/SendInvoiceEmails.php
+++ system/modules/gai_invoices/library/Bcs/Frontend/SendInvoiceEmails.php
@@ -5,13 +5,13 @@ namespace Bcs\Frontend;
 use Contao\System;
 
 
-class sendInvoiceEmails extends Contao_Frontend {
+class SendInvoiceEmails extends Contao_Frontend {
 
     protected $store_id = 0;
 
     protected $strCookie = 'FE_USER_AUTH';
 
-    public function initializeApi() {
+    public function sendEmails() {
         if (substr(\Environment::get('request'), 0, 4) == "api/") {
 
         }
