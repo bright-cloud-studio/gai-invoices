f3c60cb030d444b0e5400df8c25d7ea81b59f3be
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-04-05 10:00:49 -0400

    Update config.php

--- system/modules/gai_invoices/config/config.php
+++ system/modules/gai_invoices/config/config.php
@@ -26,7 +26,7 @@ $GLOBALS['NOTIFICATION_CENTER']['NOTIFICATION_TYPE']['gai_invoices']['send_email
 (
     'recipients'           => array('recipient_email'),
     'email_subject'        => array('recipient_name', 'invoice_number'),
-    'email_text'           => array('recipient_name', 'invoice_number', 'invoice_url'),
+    'email_text'           => array('recipient_name', 'invoice_number', 'invoice_url', 'invoice_total'),
     'email_html'           => array('recipient_name', 'invoice_number', 'invoice_url'),
     'file_name'            => array('invoice_filename', 'invoice_url'),
     'file_content'         => array('invoice_url', 'invoice_file'),
