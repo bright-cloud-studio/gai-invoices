77c68501700d78f88f7668451c13f5b6d26d9dcd
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-06-09 15:37:41 -0400

    Create tl_psychologists.php

---
+++ system/modules/gai_invoices/dca/tl_psychologists.php
@@ -0,0 +1,232 @@
+<?php
+
+/**
+ * Bright Cloud Studio's GAI Invoices
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/gai-invoices
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+ 
+/* Table tl_price_chart */
+$GLOBALS['TL_DCA']['tl_psychologists'] = array
+(
+ 
+    // Config
+    'config' => array
+    (
+        'dataContainer'               => 'Table',
+        'enableVersioning'            => true,
+        'sql' => array
+        (
+            'keys' => array
+            (
+                'id' 	=> 	'primary',
+                'alias' =>  'index'
+            )
+        )
+    ),
+ 
+    // List
+    'list' => array
+    (
+        'sorting' => array
+        (
+            // Mode 2 - Records are sotrted by a switchable field
+            // Flag 12 - Sort descending
+            'mode'                    => 2,
+            'flag'                    => 11,
+            'fields'                  => array('name'),
+            'panelLayout'             => 'sort,filter;search,limit'
+        ),
+        'label' => array
+        (
+            'fields'                  => array('name'),
+            'format'                  => '%s'
+        ),
+        'global_operations' => array
+        (
+            'all' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['MSC']['all'],
+                'href'                => 'act=select',
+                'class'               => 'header_edit_all',
+                'attributes'          => 'onclick="Backend.getScrollOffset()" accesskey="e"'
+            )
+        ),
+        'operations' => array
+        (
+            'edit' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_psychologists']['edit'],
+                'href'                => 'act=edit',
+                'icon'                => 'edit.gif'
+            ),
+			
+            'copy' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_psychologists']['copy'],
+                'href'                => 'act=copy',
+                'icon'                => 'copy.gif'
+            ),
+            'delete' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_psychologists']['delete'],
+                'href'                => 'act=delete',
+                'icon'                => 'delete.svg',
+                'attributes'          => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\'))return false;Backend.getScrollOffset()"'
+            ),
+            'toggle' => array
+			(
+				'label'               => &$GLOBALS['TL_LANG']['tl_psychologists']['toggle'],
+				'icon'                => 'visible.gif',
+				'attributes'          => 'onclick="Backend.getScrollOffset();return AjaxRequest.toggleVisibility(this,%s)"',
+				'button_callback'     => array('Bcs\Backend\PsychologistsBackend', 'toggleIcon')
+			),
+            'show' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_psychologists']['show'],
+                'href'                => 'act=show',
+                'icon'                => 'show.gif'
+            )
+        )
+    ),
+ 
+    // Palettes
+    'palettes' => array
+    (
+        'default'                     => '{psychologists_legend},invoices,name,address,address_2,city,state,zip,email,last_month_processed,price_tier;{publish_legend},published;'
+    ),
+ 
+    // Fields
+    'fields' => array
+    (
+        'id' => array
+        (
+		        'sql'                     	=> "int(10) unsigned NOT NULL auto_increment"
+        ),
+        'tstamp' => array
+        (
+		        'sql'                     	=> "int(10) unsigned NOT NULL default '0'"
+        ),
+        'sorting' => array
+        (
+            'sql'                    	=> "int(10) unsigned NOT NULL default '0'"
+        ),
+        'alias' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_psychologists']['alias'],
+            'exclude'                 => true,
+            'inputType'               => 'text',
+            'search'                  => true,
+            'eval'                    => array('unique'=>true, 'rgxp'=>'alias', 'doNotCopy'=>true, 'maxlength'=>128, 'tl_class'=>'w50'),
+            'save_callback' => array
+            (
+                array('Bcs\Backend\PsychologistsBackend', 'generateAlias')
+            ),
+            'sql'                     => "varchar(128) COLLATE utf8mb3_bin NOT NULL default ''"
+
+        ),
+        'invoices' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_psychologists']['invoices'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'name' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_psychologists']['name'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'address' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_psychologists']['address'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'address_2' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_psychologists']['address_2'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'city' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_psychologists']['city'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'state' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_psychologists']['state'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'zip' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_psychologists']['zip'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'email' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_psychologists']['email'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'last_month_processed' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_psychologists']['last_month_processed'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'price_tier' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_psychologists']['price_tier'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'published' => array
+        (
+            'exclude'                 => true,
+            'label'                   => &$GLOBALS['TL_LANG']['tl_psychologists']['published'],
+            'inputType'               => 'checkbox',
+            'eval'                    => array('submitOnChange'=>true, 'doNotCopy'=>true),
+            'sql'                     => "char(1) NOT NULL default ''"
+        )		
+    )
+);
