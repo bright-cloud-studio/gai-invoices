3fd0e9cba76c813fe01de42c9fa48449e7f8b7e4
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-06-12 13:01:13 -0400

    Update tl_services.php

--- system/modules/gai_invoices/languages/en/tl_services.php
+++ system/modules/gai_invoices/languages/en/tl_services.php
@@ -12,25 +12,22 @@
 
 
 /* System Buttons */
-$GLOBALS['TL_LANG']['tl_psychologists']['new']                        = array('New record', 'Add a new record');
-$GLOBALS['TL_LANG']['tl_psychologists']['show']                       = array('Record details', 'Show the details of record ID %s');
-$GLOBALS['TL_LANG']['tl_psychologists']['edit']                       = array('Edit record', 'Edit record ID %s');
-$GLOBALS['TL_LANG']['tl_psychologists']['copy']                       = array('Copy record', 'Copy record ID %s');
-$GLOBALS['TL_LANG']['tl_psychologists']['delete']                     = array('Delete record', 'Delete record ID %s');
-$GLOBALS['TL_LANG']['tl_psychologists']['toggle']                     = array('Toggle record', 'Toggle record ID %s');
+$GLOBALS['TL_LANG']['tl_services']['new']                        = array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_services']['show']                       = array('Record details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_services']['edit']                       = array('Edit record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_services']['copy']                       = array('Copy record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_services']['delete']                     = array('Delete record', 'Delete record ID %s');
 
 /* System Fields */
-$GLOBALS['TL_LANG']['tl_psychologists']['publish_legend']             = 'Publish';
-$GLOBALS['TL_LANG']['tl_psychologists']['alias']                      = array('Alias', 'Auto-generated alias.');
-$GLOBALS['TL_LANG']['tl_psychologists']['published']                  = array('Published', 'Show this record on the front end.');
+$GLOBALS['TL_LANG']['tl_services']['alias']                      = array('Alias', 'Auto-generated alias.');
 
 /* Fields */
-$GLOBALS['TL_LANG']['tl_psychologists']['services']                   = 'Service\'s Information Imported from Sheets';
-$GLOBALS['TL_LANG']['tl_psychologists']['service_code']               = array('Service Code', 'The Service Code assigned to this Service');
-$GLOBALS['TL_LANG']['tl_psychologists']['name']                       = array('Name', 'The Name of this Service');
-$GLOBALS['TL_LANG']['tl_psychologists']['psychologist_tier_1']        = array('Psychologist Tier 1', 'This Service\'s price for Psychologist Tier 1');
-$GLOBALS['TL_LANG']['tl_psychologists']['psychologist_tier_2']        = array('Psychologist Tier 2', 'This Service\'s price for Psychologist Tier 2');
-$GLOBALS['TL_LANG']['tl_psychologists']['psychologist_tier_3']        = array('Psychologist Tier 3', 'This Service\'s price for Psychologist Tier 3');
-$GLOBALS['TL_LANG']['tl_psychologists']['school_tier_1']              = array('School Tier 1', 'This Service\'s price for School Tier 1');
-$GLOBALS['TL_LANG']['tl_psychologists']['school_tier_2']              = array('School Tier 2', 'This Service\'s price for School Tier 2');
-$GLOBALS['TL_LANG']['tl_psychologists']['school_tier_3']              = array('SChool Tier 3', 'This Service\'s price for School Tier 3');
+$GLOBALS['TL_LANG']['tl_services']['services']                   = 'Service\'s Information Imported from Sheets';
+$GLOBALS['TL_LANG']['tl_services']['service_code']               = array('Service Code', 'The Service Code assigned to this Service');
+$GLOBALS['TL_LANG']['tl_services']['name']                       = array('Name', 'The Name of this Service');
+$GLOBALS['TL_LANG']['tl_services']['psychologist_tier_1']        = array('Psychologist Tier 1', 'This Service\'s price for Psychologist Tier 1');
+$GLOBALS['TL_LANG']['tl_services']['psychologist_tier_2']        = array('Psychologist Tier 2', 'This Service\'s price for Psychologist Tier 2');
+$GLOBALS['TL_LANG']['tl_services']['psychologist_tier_3']        = array('Psychologist Tier 3', 'This Service\'s price for Psychologist Tier 3');
+$GLOBALS['TL_LANG']['tl_services']['school_tier_1']              = array('School Tier 1', 'This Service\'s price for School Tier 1');
+$GLOBALS['TL_LANG']['tl_services']['school_tier_2']              = array('School Tier 2', 'This Service\'s price for School Tier 2');
+$GLOBALS['TL_LANG']['tl_services']['school_tier_3']              = array('SChool Tier 3', 'This Service\'s price for School Tier 3');
