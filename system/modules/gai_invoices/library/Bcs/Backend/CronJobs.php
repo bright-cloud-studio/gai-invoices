5273e09e0edc8bea4dbf4d2bd3be1f1d165d5ea0
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-06-12 13:49:06 -0400

    Update CronJobs.php

--- system/modules/gai_invoices/library/Bcs/Backend/CronJobs.php
+++ system/modules/gai_invoices/library/Bcs/Backend/CronJobs.php
@@ -11,7 +11,30 @@ class CronJobs extends System
     {
        
         // Add a log entry so we know things are going as expected
-        \Controller::log('GAI IMPORT: Importing from SHeets', __CLASS__ . '::' . __FUNCTION__, 'GENERAL');
-
+        \Controller::log('GAI IMPORT: Importing from Sheets', __CLASS__ . '::' . __FUNCTION__, 'GENERAL');
+        
+        // Connect to Sheets
+        
+        
+        
+        
+        // Import Psychologist data from the "Psychologists" sheet
+            
+            // If existing entry, update it
+        
+            // Otherwise, create new entry
+        
+        
+        
+        
+        // Import Services data from the "Services" sheet
+        
+            // If existing entry, update it
+        
+            // Otherwise, create new entry
+        
+        
+        
+        
     }
 }
