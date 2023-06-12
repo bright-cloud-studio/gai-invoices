c6fad5016960b5e45952dd967b365d558b83b46e
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-06-12 09:30:06 -0400

    Create tl_services.php

---
+++ system/modules/gai_invoices/languages/en/tl_services.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Bright Cloud Studio's GAI Invoices
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/gai-invoices
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+/* System Buttons */
+$GLOBALS['TL_LANG']['tl_psychologists']['new']                        = array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_psychologists']['show']                       = array('Record details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_psychologists']['edit']                       = array('Edit record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_psychologists']['copy']                       = array('Copy record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_psychologists']['delete']                     = array('Delete record', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_psychologists']['toggle']                     = array('Toggle record', 'Toggle record ID %s');
+
+/* System Fields */
+$GLOBALS['TL_LANG']['tl_psychologists']['publish_legend']             = 'Publish';
+$GLOBALS['TL_LANG']['tl_psychologists']['alias']                      = array('Alias', 'Auto-generated alias.');
+$GLOBALS['TL_LANG']['tl_psychologists']['published']                  = array('Published', 'Show this record on the front end.');
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_psychologists']['services']                   = 'Services Information';
+$GLOBALS['TL_LANG']['tl_psychologists']['service_code']               = array('Service Code', 'Psychologist\'s Name');
+$GLOBALS['TL_LANG']['tl_psychologists']['name']                       = array('Name', 'Psychologist\'s Name');
+$GLOBALS['TL_LANG']['tl_psychologists']['psychologist_tier_1']        = array('Psychologist Tier 1', 'Psychologist\'s Address');
+$GLOBALS['TL_LANG']['tl_psychologists']['psychologist_tier_2']        = array('Psychologist Tier 2', 'Psychologist\'s Address 2');
+$GLOBALS['TL_LANG']['tl_psychologists']['psychologist_tier_3']        = array('Psychologist Tier 3', 'Psychologist\'s City');
+$GLOBALS['TL_LANG']['tl_psychologists']['school_tier_1']              = array('School Tier 1', 'Psychologist\'s State');
+$GLOBALS['TL_LANG']['tl_psychologists']['school_tier_2']              = array('School Tier 2', 'Psychologist\'s ZIP');
+$GLOBALS['TL_LANG']['tl_psychologists']['school_tier_3']              = array('SChool Tier 3', 'Psychologist\'s Email');
