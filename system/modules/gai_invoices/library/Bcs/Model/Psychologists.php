6c15114624c4fae393de9e5d302e454b03b68bbe
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-06-12 11:53:40 -0400

    Create Psychologists.php

---
+++ system/modules/gai_invoices/library/Bcs/Model/Psychologists.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Bright Cloud Studio's GAI Invoices
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/gai-invoices
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+ 
+namespace Bcs\Model;
+
+class Psychologists extends \Model
+{
+	
+	/**
+	 * Table name
+	 * @var string
+	 */
+	protected static $strTable = 'tl_psychologists';
+    
+}
