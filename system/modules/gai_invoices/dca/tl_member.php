0bd4e363cb1e4d60b49f0887a7e870e074d2a84e
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-09-14 14:31:15 -0400

    Update tl_member.php

--- system/modules/gai_invoices/dca/tl_member.php
+++ system/modules/gai_invoices/dca/tl_member.php
@@ -11,7 +11,7 @@
 
  /* Extend the tl_user palettes */
 foreach ($GLOBALS['TL_DCA']['tl_member']['palettes'] as $k => $v) {
-    $GLOBALS['TL_DCA']['tl_member']['palettes'][$k] = str_replace('groups;', 'groups;{meetings_legend},meeting_options;', $v);
+    $GLOBALS['TL_DCA']['tl_member']['palettes'][$k] = str_replace('groups;', 'groups;{meetings_legend},meeting_options;{price_tier_legend},price_tier;', $v);
 }
     
 /* Add fields to tl_user */
@@ -22,4 +22,13 @@ $GLOBALS['TL_DCA']['tl_member']['fields']['meeting_options'] = array
     'inputType'               => 'checkbox',
     'options'                 => array('misc_travel_expenses' => 'Misc. Travel Expenses', 'editing_services' => 'Editing Services', 'meeting_coverage_coordinator' => 'Meeting Coverage Coordinator', 'parking' => 'Parking'),								
     'eval'                    => array('multiple'=>true, 'mandatory'=>false,'tl_class'=>'clr') 
+);
+$GLOBALS['TL_DCA']['tl_member']['fields']['price_tier'] = array
+(
+    'sql'                     => "varchar(255) NOT NULL default ''",
+    'inputType'               => 'text',
+    'default'                 => '',
+    'search'                  => true,
+    'eval'                    => array('mandatory'=>false, 'tl_class'=>'w50'),
+    'sql'                     => "varchar(255) NOT NULL default ''"
 );
