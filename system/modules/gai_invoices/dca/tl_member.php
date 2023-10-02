5b0385fb88bd25f954d8cbcdb5f6ddbd3e0803e0
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-10-02 09:12:11 -0400

    Update tl_member.php

--- system/modules/gai_invoices/dca/tl_member.php
+++ system/modules/gai_invoices/dca/tl_member.php
@@ -18,7 +18,7 @@ foreach ($GLOBALS['TL_DCA']['tl_member']['palettes'] as $k => $v) {
 $GLOBALS['TL_DCA']['tl_member']['fields']['meeting_options'] = array
 (
     'sql'                     => "varchar(255) NOT NULL default ''",
-    'label'                   => &$GLOBALS['TL_LANG']['tl_member']['meeting_options'],3
+    'label'                   => &$GLOBALS['TL_LANG']['tl_member']['meeting_options'],
     'inputType'               => 'checkbox',
     'options'                 => array('misc_travel_expenses' => 'Misc. Travel Expenses', 'editing_services' => 'Editing Services', 'manager' => 'Manager', 'parking' => 'Parking'),								
     'eval'                    => array('multiple'=>true, 'mandatory'=>false,'tl_class'=>'clr') 
