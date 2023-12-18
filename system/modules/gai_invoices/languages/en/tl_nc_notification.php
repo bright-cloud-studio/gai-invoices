18594f8df867e05cad5d17397c1d9f017d01aa98
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-12-18 12:41:01 -0500

    Update tl_nc_notification.php

--- system/modules/gai_invoices/languages/en/tl_nc_notification.php
+++ system/modules/gai_invoices/languages/en/tl_nc_notification.php
@@ -1,5 +1,5 @@
 <?php
 
 /** Notification types */
-$GLOBALS['TL_LANG']['tl_nc_notification']['type']['gai_invoices']         = 'GAI - Invoices';
-$GLOBALS['TL_LANG']['tl_nc_notification']['type']['send_email'] = array('Send Email', 'This notification type can be sent when using the Send Invoice Emails module.');
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['gai_invoices']      = 'GAI - Invoices';
+$GLOBALS['TL_LANG']['tl_nc_notification']['type']['send_email']        = array('Send Email', 'This notification type can be sent when using the Send Invoice Emails module.');
