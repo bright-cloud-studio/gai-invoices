1df819fac88369cae7317d0a893afd18d7d4e0a0
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-12-22 11:06:22 -0500

    Update ModSendInvoiceEmails.php

--- system/modules/gai_invoices/library/Bcs/Module/ModSendInvoiceEmails.php
+++ system/modules/gai_invoices/library/Bcs/Module/ModSendInvoiceEmails.php
@@ -1,13 +1,13 @@
 <?php
 
 /**
- * Bright Cloud Studio's GAI Invoices
- *
- * Copyright (C) 2022-2023 Bright Cloud Studio
- *
- * @package    bright-cloud-studio/gai-invoices
- * @link       https://www.brightcloudstudio.com/
- * @license    http://opensource.org/licenses/lgpl-3.0.html
+* Bright Cloud Studio's GAI Invoices
+*
+* Copyright (C) 2023-2024 Bright Cloud Studio
+*
+* @package    bright-cloud-studio/modal-gallery
+* @link       https://www.brightcloudstudio.com/
+* @license    http://opensource.org/licenses/lgpl-3.0.html
 **/
 
   
