74543a2e46aa7f93cad478a2bc0c0666d574de0a
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2023-12-27 09:00:48 -0500

    Update tl_module.php

--- system/modules/gai_invoices/dca/tl_module.php
+++ system/modules/gai_invoices/dca/tl_module.php
@@ -20,3 +20,9 @@ $GLOBALS['TL_DCA']['tl_module']['palettes']['mod_work_assignment_history'] 	= '{
 $GLOBALS['TL_DCA']['tl_module']['palettes']['mod_transaction_review'] 		= '{title_legend},name,headline,type;{template_legend:hide},customTpl;{expert_legend:hide},guests,cssID,space';
 $GLOBALS['TL_DCA']['tl_module']['palettes']['mod_admin_review'] 		    = '{title_legend},name,headline,type;{template_legend:hide},customTpl;{expert_legend:hide},guests,cssID,space';
 $GLOBALS['TL_DCA']['tl_module']['palettes']['mod_send_invoice_emails'] 		= '{title_legend},name,headline,type;{template_legend:hide},customTpl;{expert_legend:hide},guests,cssID,space';
+
+$GLOBALS['TL_DCA']['tl_module']['palettes']['mod_misc_travel_expenses'] 		    = '{title_legend},name,headline,type;{template_legend:hide},customTpl;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['mod_misc_billing'] 		    = '{title_legend},name,headline,type;{template_legend:hide},customTpl;{expert_legend:hide},guests,cssID,space';
+
+$GLOBALS['TL_DCA']['tl_module']['palettes']['mod_parking'] 		    = '{title_legend},name,headline,type;{template_legend:hide},customTpl;{expert_legend:hide},guests,cssID,space';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['mod_manager'] 		    = '{title_legend},name,headline,type;{template_legend:hide},customTpl;{expert_legend:hide},guests,cssID,space';
