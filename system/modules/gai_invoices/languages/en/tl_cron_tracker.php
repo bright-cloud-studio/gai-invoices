04f161ffd18031be0f13aeebf5e0234aca47e9d4
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-01-08 14:08:59 -0500

    Create tl_cron_tracker.php

---
+++ system/modules/gai_invoices/languages/en/tl_cron_tracker.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Bright Cloud Studio's GAI Invoices
+ *
+ * Copyright (C) 2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/gai-invoices
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+/* System Buttons */
+$GLOBALS['TL_LANG']['tl_cron_tracker']['new']                        = array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_cron_tracker']['show']                       = array('Record details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_cron_tracker']['edit']                       = array('Edit record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_cron_tracker']['copy']                       = array('Copy record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_cron_tracker']['delete']                     = array('Delete record', 'Delete record ID %s');
+
+/* System Fields */
+$GLOBALS['TL_LANG']['tl_cron_tracker']['alias']                      = array('Alias', 'Auto-generated alias.');
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_services']['cron_tracker_legend']            = 'Cron Tracker Fields';
+$GLOBALS['TL_LANG']['tl_services']['cron_task']                      = array('Cron Task', 'The name of the cron tasks we are tracking');
+$GLOBALS['TL_LANG']['tl_services']['last_trigger']                   = array('Last Trigger', 'Timestamp from when this task was last triggered');
