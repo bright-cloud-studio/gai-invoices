583dd4219219d637139afbf44f368bd6f3ce9661
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-01-08 14:00:06 -0500

    Create tl_cron_tracker.php

---
+++ system/modules/gai_invoices/dca/tl_cron_tracker.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ * Bright Cloud Studio's GAI Invoices
+ *
+ * Copyright (C) 2023-2024 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/gai-invoices
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+use Contao\DataContainer;
+use Contao\DC_Table;
+ 
+/* Table tl_cron_tracker */
+$GLOBALS['TL_DCA']['tl_cron_tracker'] = array
+(
+ 
+    // Config
+    'config' => array
+    (
+        'dataContainer'               => DC_Table::class,
+        'enableVersioning'            => true,
+        'sql' => array
+        (
+            'keys' => array
+            (
+                'id' 	=> 	'primary',
+                'name' =>  'index'
+            )
+        )
+    ),
+ 
+    // List
+    'list' => array
+    (
+        'sorting' => array
+        (
+            'mode'                    => DataContainer::MODE_SORTED,
+            'fields'                  => array('cron_task'),
+            'flag'                    => DataContainer::SORT_INITIAL_LETTER_ASC,
+            'panelLayout'             => 'filter;search,limit',
+            'defaultSearchField'      => 'name'
+        ),
+        'label' => array
+        (
+            'fields'                  => array('cron_task'),
+            'format'                  => '<span style="font-weight: bold;">Cron Task: </span> %s'
+        ),
+        'global_operations' => array
+        (
+            'all' => array
+            (
+                'href'                => 'act=select',
+                'class'               => 'header_edit_all',
+                'attributes'          => 'onclick="Backend.getScrollOffset()" accesskey="e"'
+            )
+        ),
+        'operations' => array
+        (
+            'edit' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_cron_tracker']['edit'],
+                'href'                => 'act=edit',
+                'icon'                => 'edit.gif'
+            ),
+			
+            'copy' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_cron_tracker']['copy'],
+                'href'                => 'act=copy',
+                'icon'                => 'copy.gif'
+            ),
+            'delete' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_cron_tracker']['delete'],
+                'href'                => 'act=delete',
+                'icon'                => 'delete.svg',
+                'attributes'          => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\'))return false;Backend.getScrollOffset()"'
+            ),
+            'show' => array
+            (
+                'label'               => &$GLOBALS['TL_LANG']['tl_cron_tracker']['show'],
+                'href'                => 'act=show',
+                'icon'                => 'show.gif'
+            )
+        )
+    ),
+ 
+    // Palettes
+    'palettes' => array
+    (
+        'default'                       => '{cron_tracker_legend},cron_task,last_trigger;'
+    ),
+ 
+    // Fields
+    'fields' => array
+    (
+        'id' => array
+        (
+		        'sql'                   => "int(10) unsigned NOT NULL auto_increment"
+        ),
+        'tstamp' => array
+        (
+		        'sql'                   => "int(10) unsigned NOT NULL default '0'"
+        ),
+        'sorting' => array
+        (
+            'sql'                    	=> "int(10) unsigned NOT NULL default '0'"
+        ),
+        'cron_task' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_cron_tracker']['cron_task'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        ),
+        'last_trigger' => array
+        (
+            'label'                   => &$GLOBALS['TL_LANG']['tl_cron_tracker']['last_trigger'],
+            'inputType'               => 'text',
+            'default'                 => '',
+            'search'                  => true,
+            'eval'                    => array('mandatory'=>true, 'tl_class'=>'w50'),
+            'sql'                     => "varchar(255) NOT NULL default ''"
+        )
+    )
+);
