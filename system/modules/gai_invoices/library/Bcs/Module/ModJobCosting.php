332fba75e7353147d8f39b43059db90f8d9c0241
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-01-29 13:48:03 -0500

    Create ModJobCosting.php

---
+++ system/modules/gai_invoices/library/Bcs/Module/ModJobCosting.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+* Bright Cloud Studio's GAI Invoices
+*
+* Copyright (C) 2023-2024 Bright Cloud Studio
+*
+* @package    bright-cloud-studio/modal-gallery
+* @link       https://www.brightcloudstudio.com/
+* @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+  
+namespace Bcs\Module;
+use Google;
+ 
+class ModJobCosting extends \Contao\Module
+{
+ 
+    /**
+     * Template3
+     * @var string
+     */
+    protected $strTemplate = 'mod_job_costing';
+  
+    // our google api stuffs
+    protected $client;
+    protected $service;
+    public static $spreadsheetId;
+ 
+	/**
+	 * Initialize the object
+	 *
+	 * @param \ModuleModel $objModule
+	 * @param string       $strColumn
+	 */
+	public function __construct($objModule, $strColumn='main')
+	{
+        parent::__construct($objModule, $strColumn);
+
+        // Create a client connection to Google
+        $this->$client = new Google\Client();
+        // Load our auth key
+        $this->$client->setAuthConfig('key.json');
+        // Set our scope to use the Sheets service
+        $this->$client->addScope(Google\Service\Sheets::SPREADSHEETS);
+        // Assign our client to a service
+        $this->$service = new \Google_Service_Sheets($this->$client);
+        // Set the ID for our specific spreadsheet
+        ModJobCosting::$spreadsheetId = '1erZUWlCgpWd67E1PIwwKNCYT0yCm2QiV2DL28VA8oVU';
+	}
+	
+    /**
+     * Display a wildcard in the back end
+     * @return string
+     */
+    public function generate()
+    {
+        if (TL_MODE == 'BE')
+        {
+            $objTemplate = new \BackendTemplate('be_wildcard');
+ 
+            $objTemplate->wildcard = '### ' . utf8_strtoupper($GLOBALS['TL_LANG']['FMD']['job_costing'][0]) . ' ###';
+            $objTemplate->title = $this->headline;
+            $objTemplate->id = $this->id;
+            $objTemplate->link = $this->name;
+            $objTemplate->href = 'contao/main.php?do=themes&table=tl_module&act=edit&id=' . $this->id;
+ 
+            return $objTemplate->parse();
+        }
+ 
+        return parent::generate();
+    }
+    
+ 
+    /* Generate the module */
+    protected function compile()
+    {
+        // Import the Database stuffs so we can make queries
+        $this->import('Database');
+        
+        // Include our JS with a unique code to prefent caching
+        $rand_ver = rand(1,9999);
+        $GLOBALS['TL_BODY']['work_assignments'] = '<script src="system/modules/gai_invoices/assets/js/gai_invoice.js?v='.$rand_ver.'"></script>';
+        
+        // get the user and build their name
+        $objUser = \FrontendUser::getInstance();
+        $user = $objUser->firstname . " " . $objUser->lastname;
+        
+        // Get this user's unprocessed listings from Sheets
+        $spreadsheet = $this->$service->spreadsheets->get(ModWorkAssignments::$spreadsheetId);
+        
+        // LIVE
+        $range = 'SY2022-2023';
+        
+        $response = $this->$service->spreadsheets_values->get(ModWorkAssignments::$spreadsheetId, $range);
+        $values = $response->getValues();
+	}
+
+} 
